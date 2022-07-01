d504e42c6b79219b73eea70787972129ac0dbe17
tejas <tejasshah@gmail.com>
2022-07-01 17:51:35 +0530

    table creation - basic prop

---
+++ src/main/java/com/SpringBootWithJpaHbApplication.java
@@ -0,0 +1,13 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWithJpaHbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWithJpaHbApplication.class, args);
+	}
+
+}
