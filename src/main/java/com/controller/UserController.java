816a1debf6aecebe79a655caad6178c1663e584e
tejas <tejasshah@gmail.com>
2022-07-03 14:40:53 +0530

    crud

--- src/main/java/com/controller/UserController.java
+++ src/main/java/com/controller/UserController.java
@@ -1,11 +1,15 @@
 package com.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.entity.UserEntity;
@@ -31,4 +35,24 @@ public class UserController {
 		List<UserEntity> users = userRepo.findAll();
 		return users;
 	}
+
+	@DeleteMapping
+	public boolean removeUser(@RequestParam("userId") int userId) {
+		Optional<UserEntity> user = userRepo.findById(userId);
+		if (user.isPresent()) {
+			userRepo.deleteById(userId);
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	@PutMapping
+	public UserEntity updateUser(UserEntity user) {
+		userRepo.save(user);//id fn ln 
+		return user;
+	}
+
+
 }
+
