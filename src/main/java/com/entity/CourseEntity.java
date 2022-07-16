3d238dee990f8a2ad4d29fc08527d7c8a0c8dd04
tejas <tejasshah@gmail.com>
2022-07-16 16:56:55 +0530

    m to m

---
+++ src/main/java/com/entity/CourseEntity.java
@@ -0,0 +1,47 @@
+package com.entity;
+
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "course")
+public class CourseEntity {
+
+	@Id
+	@GeneratedValue
+	int courseId;
+	String courseName;
+
+	@ManyToMany(mappedBy = "courses")
+	List<StudentEntity> students;
+
+	public int getCourseId() {
+		return courseId;
+	}
+
+	public void setCourseId(int courseId) {
+		this.courseId = courseId;
+	}
+
+	public String getCourseName() {
+		return courseName;
+	}
+
+	public void setCourseName(String courseName) {
+		this.courseName = courseName;
+	}
+
+	public List<StudentEntity> getStudents() {
+		return students;
+	}
+
+	public void setStudents(List<StudentEntity> students) {
+		this.students = students;
+	}
+
+}
---
+++ src/main/java/com/entity/StudentEntity.java
@@ -0,0 +1,59 @@
+package com.entity;
+
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "students")
+public class StudentEntity {
+
+	@Id
+	@GeneratedValue
+	int studentId;
+	String firstName;
+	String email;
+
+	@ManyToMany
+	@JoinTable(name = "student_course", joinColumns = @JoinColumn(name = "studentId"), inverseJoinColumns = @JoinColumn(name = "courseId"))
+	List<CourseEntity> courses;
+
+	public int getStudentId() {
+		return studentId;
+	}
+
+	public void setStudentId(int studentId) {
+		this.studentId = studentId;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public List<CourseEntity> getCourses() {
+		return courses;
+	}
+
+	public void setCourses(List<CourseEntity> courses) {
+		this.courses = courses;
+	}
+
+}
